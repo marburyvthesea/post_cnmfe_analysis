ba9765a45eff3c0d3694c319590cf3b915f00759
chief <chief>
2020-04-09 12:00:20 -0500

    neuron viewer changes

--- neuron_viewer/neuron_viewer_main.py
+++ neuron_viewer/neuron_viewer_main.py
@@ -5,6 +5,7 @@
 from tkinter import *
 import cv2
 import numpy as np
+import time
 
 
 ## add tkinter module for selecting matlab output file
@@ -22,19 +23,31 @@ import numpy as np
 
 
 processed_video_file = '/Volumes/My_Passport/cnmfe_analysis_files/GRIN034/H21_M22_S45/H21_M22_S45msCam1.tif'
-processed_vid_cap = cv2.VideoCapture(processed_video_file)
+#processed_vid_cap = cv2.VideoCapture(processed_video_file)
 
+success, images = cv2.imreadmulti(processed_video_file)
 cv2.namedWindow('demixed video')
 
-while processed_vid_cap.isOpened():
-    err, img = processed_vid_cap.read()
+print(len(images[1]))
 
-    cv2.imshow("demixed video", img)
+#cv2.imshow('demixed video', images[2])
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
-    k = cv2.waitKey(10) & 0xff
+frame_count = len(images)
+i = 0
+while i<frame_count:
+  #  err, img = processed_vid_cap.read()
 
-    if k==27:
-        break
+    cv2.imshow("demixed video", images[i])
+    cv2.waitKey(1)
+    #time.sleep(1)
+    print(i)
+    i+=1
+ #  k = cv2.waitKey(10) & 0xff
+
+ #   if k==27:
+ #       break
 
 
 
