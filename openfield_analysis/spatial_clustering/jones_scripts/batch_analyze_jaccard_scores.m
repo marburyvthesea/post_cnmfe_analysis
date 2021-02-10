afbacb3b930d273997d4b809355bd0fb934972ba
chief <chief>
2021-02-10 16:45:29 -0600

    changes

--- openfield_analysis/spatial_clustering/jones_scripts/batch_analyze_jaccard_scores.m
+++ openfield_analysis/spatial_clustering/jones_scripts/batch_analyze_jaccard_scores.m
@@ -6,16 +6,17 @@
 
 %base data dir
 
-dataDirectory = '/projects/p30771/miniscope/analysis/OpenFieldAnalysis/spatial_clusters/jones_script_analysis/data/ko/' ; 
+%dataDirectory = '/projects/p30771/miniscope/analysis/OpenFieldAnalysis/spatial_clusters/jones_script_analysis/data/ko/' ; 
 
-WT_sessions = {'GRIN013_H13_M33_S54'; 'GRIN026_H16_M35_S34'; 'GRIN027_H13_M29_S44'; 'GRIN032_H16_M49_S22'; 'GRIN032_H17_M30_S22'; 'GRIN033_H13_M42_S33'; ...
-			'GRIN033_H14_M34_S32'; 'GRIN034_H15_M28_S2'; 'GRIN034_H16_M13_S22'; 'GRIN034_H19_M0_S18'; 'GRIN035_H13_M31_S20'; 'GRIN035_H13_M50_S58'; 'GRIN035_H14_M40_S34'} ; 
+%WT_sessions = {'GRIN013_H13_M33_S54'; 'GRIN026_H16_M35_S34'; 'GRIN027_H13_M29_S44'; 'GRIN032_H16_M49_S22'; 'GRIN032_H17_M30_S22'; 'GRIN033_H13_M42_S33'; ...
+%			'GRIN033_H14_M34_S32'; 'GRIN034_H15_M28_S2'; 'GRIN034_H16_M13_S22'; 'GRIN034_H19_M0_S18'; 'GRIN035_H13_M31_S20'; 'GRIN035_H13_M50_S58'; 'GRIN035_H14_M40_S34'} ; 
 
-KO_sessions = {'GRIN009_H13_M59_S14'; 'GRIN011_H10_M19_S59'; 'GRIN012_H16_M57_S23'; 'GRIN012_H17_M32_S17'; ...
-			 'GRIN018_H16_M13_S53'; 'GRIN018_H17_M41_S43'} ;
+%KO_sessions = {'GRIN009_H13_M59_S14'; 'GRIN011_H10_M19_S59'; 'GRIN012_H16_M57_S23'; 'GRIN012_H17_M32_S17'; ...
+%			 'GRIN018_H16_M13_S53'; 'GRIN018_H17_M41_S43'} ;
+%input sessions
 
 %loop over sessions
-size_array = size(KO_sessions);
+size_array = size(sessions);
 
 %structure to store data
 batchData = cell(size_array(1,1), 1);
