2623b1d53e56ed1d056701dfb36947fdb00a3955
chief <chief>
2021-02-10 16:45:29 -0600

    changes

--- openfield_analysis/spatial_clustering/jones_scripts/jaccard_compute_fn_jjm.m
+++ openfield_analysis/spatial_clustering/jones_scripts/jaccard_compute_fn_jjm.m
@@ -12,23 +12,26 @@ save_path = strcat(dataDir, session, '_');
 %load filtered fluorescence traces from python output
 disp('loading data')
 disp(session)
-cell_eg = readtable(strcat(dataDir,session,'_filtered_f_traces.csv'),'ReadVariableNames', true);
+cell_eg = readtable(strcat(dir_path,session,'_C_traces_filtered.csv'),'ReadVariableNames', true);
 
 %cell centroids 
 cellXYcoords = csvread(strcat(dataDir,session,'_com.csv'),1, 1);
 
 %remove nonnumeric variables
-cell_eg_numeric = removevars(cell_eg,{'Var1','msCamFrame','velocity_bins'});
+%cell_eg_numeric = removevars(cell_eg,{'Var1','msCamFrame','velocity_bins'});
 
 %convert to array
-cell_traces = table2array(cell_eg_numeric);
+%remove 1st column, which is just index 
+size_array = size(cell_eg);
+
+cell_traces = table2array(cell_eg(:,2:size_array(1,2)));
 %convert to nCells x nFrames matrix
 cell_traces = cell_traces';
 
 disp('finding signal peaks')
 
-%%get "peaks" in signal, F/F0 above a, here 2.5, SD threshold
-[signalPeaks, ~, ~] = computeSignalPeaks(cell_traces, 'doMovAvg', 0, 'reportMidpoint', 1, 'numStdsForThresh', sdThreshold);
+% get "peaks" in signal, F/F0 above a, here 2.5, SD threshold
+[signalPeaks, ~, ~] = computeSignalPeaks(cell_traces, 'doMovAvg', 0, 'reportMidpoint', 1, 'numStdsForThresh', 2.5);
 
 %in the PD paper, we pad each 'event' to make is 1-s duration. Note that we
 %no longer do this with our GCaMP7f data, but I would do it with GCaMP6
@@ -36,6 +39,7 @@ disp('finding signal peaks')
 
 disp('padding signal peaks')
 
+%should adjust padded signal peaks to work with different input sampling intervals 
 paddedSignalPeaks = getPaddedSignalPeaks(signalPeaks);
 
 
@@ -43,13 +47,19 @@ paddedSignalPeaks = getPaddedSignalPeaks(signalPeaks);
 %note that in our movies, the pixel size is 2.5um. We can take your
 %microscope and image a grid slide to determine the pixel size of your
 %microscope after all the processing steps.
-
-cellDistances = pdist(cellXYcoords, 'euclidean')*pixelScale;
 %distances multiplied by pixels/microns
 % f=12, 0.9
 % f=15, 0.96
 % f=20, 1.02
 
+%convert cellXYcoords table to array
+size_com_table = size(cellXYcoords);
+XY_coords_array = table2array(cellXYcoords(:,2:size_com_table(1,2))); 
+XY_coords_array = XY_coords_array';
+
+cellDistances = pdist(XY_coords_array, 'euclidean')*1;%distances multiplied by 2.5 = microns
+%ouput squareform array for comparison in python later 
+cellDistances_squareform = squareform(cellDistances); 
 
 %we often want to compare treatments and behavioral states (e.g., periods
 %of movement during amphetamine treatment). I am just going to analyse all
@@ -57,6 +67,7 @@ cellDistances = pdist(cellXYcoords, 'euclidean')*pixelScale;
 %speed (which I would recommend, because the measurement appears to be
 %higher at rest and lower at higher running speeds). We can look closer at
 %this later. 
+
 
 frames_to_analyze = 1: size(cell_traces, 2);
 within_frames_to_analyze = 1: size(cell_traces, 2);
@@ -68,7 +79,7 @@ within_frames_to_analyze = 1: size(cell_traces, 2);
 %think this is a huge program with CNMFe, because it merges these cells)
 
 numBins = maxDist/binSize-1;%throw out the first bin (overlapping cells)
-binVector = 20:binSize:maxDist;%cells must be atleast 20um apart. 
+binVector = binSize:binSize:maxDist;%cells must be atleast 20um apart. 
 numFrames = length(signalPeaks);
 numCells = size(cellXYcoords, 1);
 
@@ -86,6 +97,7 @@ csvwrite(strcat(save_path,'ShuffledCellJaccards','.csv'), ShuffledCellJaccards);
 csvwrite(strcat(save_path,'normlBinnedCellJaccards','.csv'), normlBinnedCellJaccards);
 csvwrite(strcat(save_path,'normlShuffledBinnedCellJaccards','.csv'), normlShuffledBinnedCellJaccards);
 csvwrite(strcat(save_path,'proximalPairIndices','.csv'), proximalPairIndices);
+csvwrite(strcat(save_path,'cellDistances_squareform_matrix','.csv'), cellDistances_squareform);
 
 %% save parameters to structure
 analysisParams = struct('session', session, 'sdThreshold', sdThreshold, 'pixelScaleFactor', pixelScale, 'maxDistanceForBinning', ... 
