ca304a64a8ed66e4fc3616ac101b9a528e9e7d0b
chief <chief>
2021-02-25 11:11:58 -0600

    changes

--- dlc_utils.py
+++ dlc_utils.py
@@ -18,8 +18,9 @@ def get_resting_period_boundaries(trace_mask):
 			while sample < len(trace_mask) and trace_mask[sample]==True:
 				sample+1
 			rest_offset.append(sample) 
-		print(sample)
-		
+		else:
+			sample+=1
+
 	resting_boundaries_indicies = list(zip(rest_onset, rest_offset))
 	return(resting_boundaries_indicies)
 
